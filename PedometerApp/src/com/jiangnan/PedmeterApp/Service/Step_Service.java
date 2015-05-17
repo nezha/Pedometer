0f01fd7b62efea30011764da09e274d77b7c8ee3
zhang yi <1061243497@qq.com>
2015-05-17 23:32:34 +0800

    20150517 update alg using kalman ,FSM,Threshold,CZ

--- PedometerApp/src/com/jiangnan/PedmeterApp/Service/Step_Service.java
+++ PedometerApp/src/com/jiangnan/PedmeterApp/Service/Step_Service.java
@@ -1,14 +1,18 @@
 package com.jiangnan.PedmeterApp.Service;
 
+import com.jiangnan.PedmeterApp.R;
 import com.jiangnan.PedmeterApp.Util.SensorUtil;
-import com.jiangnan.PedmeterApp.Util.StepDetector;
-
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.widget.Toast;
 
 
@@ -21,6 +25,9 @@ import android.widget.Toast;
  */
 public class Step_Service extends Service implements SensorEventListener{
 	SensorUtil SU;
+	
+	private PowerManager.WakeLock wakeLock;
+    private NotificationManager mNM;
 
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -33,6 +40,14 @@ public class Step_Service extends Service implements SensorEventListener{
 		// TODO Auto-generated method stub
 		super.onCreate();
 		Toast.makeText(this, "--service started--", Toast.LENGTH_LONG).show();
+		
+		mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        //showNotification();
+        
+        acquireWakeLock();
+        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mReceiver, filter);
+        
 		SU = new SensorUtil(this);
 		SU.registerListener();
 		
@@ -41,8 +56,12 @@ public class Step_Service extends Service implements SensorEventListener{
 	@Override
 	public void onDestroy() {
 		// TODO Auto-generated method stub
-		super.onDestroy();
 		SU.unregisterListener();
+		
+		mNM.cancel(R.string.app_name);
+        wakeLock.release();
+		super.onDestroy();
+		
 	}
 
 	@Override
@@ -58,15 +77,62 @@ public class Step_Service extends Service implements SensorEventListener{
 	}
 
 	@Override
-	public void onSensorChanged(SensorEvent e) {
+	public void onSensorChanged(final SensorEvent e) {
 		// TODO Auto-generated method stub
-		SU.routeEvent(e);
-		//Toast.makeText(this, StepDetector.CURRENT_SETP, Toast.LENGTH_LONG).show();
-		//System.out.println("--"+StepDetector.CURRENT_SETP);
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				SU.routeEvent(e);
+			}
+		}).start();
+		
 	}
 
+	/**
+     * Show a notification while this service is running.
+     */
+	/*
+    private void showNotification() {
+        CharSequence text = getText(R.string.app_name);
+        Notification notification = new Notification(R.drawable.ic_notification, null,
+                System.currentTimeMillis());
+        notification.flags = Notification.FLAG_NO_CLEAR | Notification.FLAG_ONGOING_EVENT;
+        Intent pedometerIntent = new Intent();
+        pedometerIntent.setComponent(new ComponentName(this, MainPageActivity.class));
+        pedometerIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                pedometerIntent, 0);
+        notification.setLatestEventInfo(this, text,
+                getText(R.string.notification_subtitle), contentIntent);
+
+        mNM.notify(R.string.app_name, notification);
+    }
+    */	
 	
-	
+    // BroadcastReceiver for handling ACTION_SCREEN_OFF.
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Check action just to be on the safe side.
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                // Unregisters the listener and registers it again.
+                SU.unregisterListener();
+                SU.registerListener();
+                    wakeLock.release();
+                    acquireWakeLock();
+            }
+        }
+    };
+    
+    private void acquireWakeLock() {
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        int wakeFlags;
+        wakeFlags = PowerManager.PARTIAL_WAKE_LOCK;
+        wakeLock = pm.newWakeLock(wakeFlags, "lock");
+        wakeLock.acquire();
+    }
 	
 
 }
--- PedometerApp/src/com/jiangnan/PedmeterApp/Util/StepDetector.java
+++ PedometerApp/src/com/jiangnan/PedmeterApp/Util/StepDetector.java
@@ -1,5 +1,7 @@
 package com.jiangnan.PedmeterApp.Util;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -24,8 +26,121 @@ public class StepDetector{
 	private static float mLastDiff[] = new float[3 * 2];
 	private static int mLastMatch = -1;
 	
+	/**过零检测用的数据**/
+	static int cz_wcount = 0;
+	static int cz_i = 0;
+	static double zero = 9.8;
+	static int windowsCz = 50;
+	
+	/**阈值检测的数据**/
+	static int thr_wcount = 0;
+	static int thr_i = 0;
+	static double threshold = 12.0;
+	static int windowsThr = 40;
+	
+	/**卡尔曼参数**/
+	static int klm_count = 0;//用于判断初始化
+	static double A=1;      //状态转移系数
+	static double H=1;      //量测系数
+	static double Q=0.01; 	//激励噪声协方差
+	//测量噪声协方差。R设置地越小，滤波结果越接近于原始数据；R越大，正弦特征越明显，但此时的滤波结果与原始数据差距越大
+	static double R = 0.1;
+	private static ArrayList<Double> x_posteri = new ArrayList<Double>();
+	//初始化    
+	static double cov_posteri = 1.0; //初始化后验误差方差
+	
+	
+	/**FSM参数**/
+	static int fsm_count = 0;
+	static double pp_static = 11.5; //三状态中的峰值，经验值
+	static double np_static = 9; //谷值，经验值
+    static double Thr = 9.8;
+    static double Neg_Thr = 9.8;
+    static int currentState = 1;
+	
+	
 	public static void accelerationUpdate(SensorEvent event)
 	{
+		/**先频率融合**/
+		double acc = Math.sqrt(Math.pow(event.values[0], 2)+Math.pow(event.values[1], 2)+Math.pow(event.values[2], 2));
+		
+		/**先滤波，klm滤波**/
+		//如果是第一次需要初始化数据
+		if(klm_count == 0)
+		{
+			x_posteri.add(9.8);
+		}
+        //Predictor equations
+        double x_priori =A*x_posteri.get(klm_count);   //向前推算状态变量
+        double residual=acc-H*x_priori;           //测量残余 
+        double cov_priori=A*A*cov_posteri+Q;//向前推算误差协方差。注意cov_posteri在后面经过了一次计算，也就是说这里的值是上一次(k-1)循环带来的
+        //Corrector equations
+        double k=H*cov_priori/(H*H*cov_priori+R); 	//计算卡尔曼增益
+        cov_posteri=cov_priori*(1-H*k);     //更新误差协方差
+        x_posteri.add(x_priori+k*residual);	//更新估计，即滤波结果。这是下面调用的加速度基础
+        klm_count++;
+        //************************************************//
+        
+        //////////////////下面三者选其一，其他注释掉///////////////////////////////
+        /*
+        //过零检测算法
+        if(cz_wcount<=0 && (x_posteri.get(cz_i) < zero && x_posteri.get(cz_i+1) >= zero))
+        {
+            cz_wcount = windowsCz;
+            CURRENT_SETP++;
+        }else{
+        	cz_wcount--;
+        }
+        cz_i++;
+		*/
+		/*
+        ////////////////阈值检测算法
+        if(thr_wcount<=0 && (x_posteri.get(thr_i) < threshold && x_posteri.get(thr_i+1) >= threshold))
+        {
+            thr_wcount = windowsThr;
+            CURRENT_SETP++;
+        }else{
+        	thr_wcount--;
+        }
+        thr_i++;
+        */
+        
+        //////////////3-FSM
+        double input = x_posteri.get(fsm_count);
+        fsm_count++;
+        //S1
+        if(currentState == 1) //初始状态（静止状态）
+        {
+            if (input > pp_static)
+                currentState = 2;  //转入峰值状态	
+        }
+        //S2
+        if(currentState == 2)
+        {
+            if(input < np_static)
+            {
+                currentState = 3; //装入谷值状态
+                CURRENT_SETP++;
+             }	
+        }
+        //S3
+        if(currentState == 3) //由谷值转入静止状态
+        {
+            if(input >= Thr)
+                currentState = 1;	
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+		/*
+		 * 这是谷歌到的算法
 		if(CURRENT_SETP == 0)
 		{
 			int h = 480;
@@ -75,5 +190,17 @@ public class StepDetector{
 				mLastDirections[k] = direction;
 				mLastValues[k] = v;
 			}
-	}
+			*/
+	} 
 }
+
+
+
+
+
+
+
+
+
+
+
